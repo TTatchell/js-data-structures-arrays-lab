2841cdcb349b7cb54a4f1b80157ab4ce3569fa8c
TTatchell <tim.tatchell24@gmail.com>
2021-03-28 17:52:49 +1100

    uplopad

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+    return drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    return drivers.unshift(name);
+
+}
+
+function destructivelyRemoveLastDriver() {
+    return drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver() {
+    return drivers.shift();
+}
+
+function appendDriver(name) {
+    const newDrivers = [...drivers];
+    newDrivers.push(name);
+    return newDrivers;
+}
+
+function prependDriver(name) {
+    const newDrivers = [...drivers];
+    newDrivers.unshift(name);
+    return newDrivers;
+}
+
+function removeLastDriver() {
+    const newDrivers = [...drivers];
+    newDrivers.pop();
+    return newDrivers;
+}
+
+function removeFirstDriver() {
+    const newDrivers = [...drivers];
+    newDrivers.shift();
+    return newDrivers;
+}
